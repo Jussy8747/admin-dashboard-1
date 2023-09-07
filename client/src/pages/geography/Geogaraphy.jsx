dcf47a25c07658957a249c89b32a14b01413956f
Justice Duruanyim <uzochiduru8747@gmail.com>
2023-09-07 06:31:39 +0100

    edited the transaction and geograpyh pages

---
+++ client/src/pages/geography/Geogaraphy.jsx
@@ -0,0 +1,9 @@
+import { Box, useTheme } from "@mui/material";
+import Header from "../../component/Header";
+import { useGetGeographyQuery } from "../../slices/globalSliceApi";
+
+const Geogaraphy = () => {
+  return <div>Geogaraphy</div>;
+};
+
+export default Geogaraphy;
